64bdddad1acd8fca330be1af69ff87dfc59f4e37
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2023-11-16 11:25:44 -0800

    Adding remaining provided HW8 files

---
+++ ViewModels/RowViewModel.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using My321HW4.ViewModels;
+using ReactiveUI;
+
+namespace SpreadsheetEngine.ViewModels;
+
+public class RowViewModel : ViewModelBase
+{
+    public RowViewModel(List<CellViewModel> cells)
+    {
+        Cells = cells;
+        foreach (var cell in Cells)
+        {
+            cell.PropertyChanged += CellOnPropertyChanged;
+        }
+        SelfReference = this;
+    }
+    public CellViewModel this[int index] => Cells[index];
+    private void CellOnPropertyChanged(object? sender, PropertyChangedEventArgs
+        args)
+    {
+        var styleImpactingProperties = new List<string>
+        {
+            nameof(CellViewModel.IsSelected),
+            nameof(CellViewModel.CanEdit),
+            nameof(CellViewModel.BackgroundColor),
+        };
+        if (styleImpactingProperties.Contains(args.PropertyName))
+            FireChangedEvent();
+    }
+    public List<CellViewModel> Cells { get; set; }
+    /// <summary>
+    /// This property provides a way to notify the value converter that it needs to update
+    /// </summary>
+    public RowViewModel SelfReference { get; private set; }
+    public void FireChangedEvent()
+    {
+        this.RaisePropertyChanged(nameof(SelfReference));
+    }
+}
